e5f0eeae790690102df2b7ca4ec7a96ee7cdda05
Abesh <abeshsarkar2030@gmail.com>
2024-01-30 02:37:34 +0530

    updation

--- script.js
+++ script.js
@@ -1,44 +1,67 @@
-let jokes = document.querySelector("#joke");
-let btn = document.querySelector("#joke-change")
-btn.addEventListener("click", jokeChange);
+// let jokes = document.querySelector("#joke");
+// let btn = document.querySelector("#joke-change")
+// btn.addEventListener("click", clickModular);
 
-// function jokeChange() {
-//     let empty = " ";
+// function clickModular(){
+//     let joke = " ";
 
 //     const xhr = new XMLHttpRequest();
-//     xhr.open("GET", "https://api.api-ninjas.com/v1/dadjokes?limit=1");
-//     xhr.setRequestHeader("X-Api-key", "/lmdfFM2+KXR+MouNOsnrUw==pbQhlpDwI7LskIIo");
+//     xhr.open('GET', "https://api.api-ninjas.com/v1/dadjokes?limit=1");
+//     xhr.setRequestHeader('X-Api-key', "/kKbBkDCP6NzStP5TmuARQ==weULnBZuaArhNg8n");
 //     xhr.send();
-//     btn.innerText = "loading..."
+
+//     btn.innerText = "Fetching jokes...";
+
 //     xhr.onload = function(){
 //         if(xhr.status === 200){
 //             const result = JSON.parse(xhr.responseText);
-//             empty = result[0].empty;
-//             jokes.innerText = empty;
-//             btn.innerText = "tell me a joke";
+//             console.log(result);
+//             joke = result[0].joke;
+            
+//             // update the jokes in
+//             jokes.innerText = joke;
+
+//             btn.innerText = "Tell Me A Joke";
 //         }
-//     };
+//     }
 // }
 
-function clickModular(){
-    let joke = " ";
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', "https://api.api-ninjas.com/v1/dadjokes?limit=1");
-    xhr.setRequestHeader('x-Api-key', "/lmdfFM2+KXR+MouNOsnrUw==pbQhlpDwI7LskIIo");
-    xhr.send();
+// let getUrl ="https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+// async function clickModular () {
+//     let joke = " ";
+
+//     let request = await fetch(getUrl);
+//     let get = await request.json()
+// }
+
+let jokes = document.querySelector("#joke");
+let btn = document.querySelector("#joke-change");
+
+btn.addEventListener("click", clickModular);
+
+async function clickModular() {
+    let joke = " ";
 
     btn.innerText = "Fetching jokes...";
 
-    xhr.onload = function(){
-        if(xhr.status === 200){
-            const result = JSON.parse(xhr.responseText);
-            joke = result[0].joke;
-            
-            // update the jokes in
-            jokes.innerText = joke;
-
-            btn.innerText = "Tell Me A Joke";
+    const response = await fetch("https://api.api-ninjas.com/v1/dadjokes?limit=1", {
+        headers: {
+            'X-Api-key': '/kKbBkDCP6NzStP5TmuARQ==weULnBZuaArhNg8n'
         }
-    };
+    });
+
+    if (response.ok) {
+        const result = await response.json();
+        joke = result[0].joke;
+        // console.log(joke)
+        jokes.innerText = joke;
+    } else {
+        console.error('Network response was not ok');
+        jokes.innerText = "Failed to fetch joke";
+    }
+
+    btn.innerText = "Tell Me A Joke";
 }
+
